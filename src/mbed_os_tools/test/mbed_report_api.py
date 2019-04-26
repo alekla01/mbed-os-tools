eaece3983013226763e5a3619baec57968144a40
Aleksi Klasila <aleksi.klasila@arm.com>
2019-04-26 13:30:39 +0300

    Update mbed_report_api.py

--- src/mbed_os_tools/test/mbed_report_api.py
+++ src/mbed_os_tools/test/mbed_report_api.py
@@ -40,6 +40,7 @@ def exporter_json(test_result_ext, test_suite_properties=None):
                 suite["single_test_output"] = suite["single_test_output"].decode("utf-8")
             except KeyError:
                 pass
+    raise Exception(test_result_ext)
     return json.dumps(list(test_result_ext), indent=4)
 
 
